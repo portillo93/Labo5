bf0db998f8518ae888ff8fea50cf3b9f8536e7d2
UCA-PC-DEI <alejandrojuarezck@gmail.com>
2018-04-17 12:51:15 -0600

    Initial commit

---
+++ app/src/main/java/com/example/uca/labo5/Serie.java
@@ -0,0 +1,49 @@
+package com.example.uca.labo5;
+
+/**
+ * Created by uca on 04-17-18.
+ */
+
+public class Serie {
+    private String name, caps, desc;
+    private int img;
+
+    public Serie(String name, String caps, int img,String desc){
+        this.name = name;
+        this.caps = caps;
+        this.img = img;
+        this.desc = desc;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCaps() {
+        return caps;
+    }
+
+    public void setCaps(String caps) {
+        this.caps = caps;
+    }
+
+    public int getImg() {
+        return img;
+    }
+
+    public void setImg(int img) {
+        this.img = img;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
